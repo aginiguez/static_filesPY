e92ba882eb6bea6945888c305ed39e6d79b91da6
Your Name <youremail@example.com>
2024-06-13 22:19:14 -0700

    final edits

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,14 +1,19 @@
-# config                    
 from flask import Flask
 
-# factory
 def create_app():
     app = Flask(__name__)
 
-    # index route
     @app.route('/')
-    def index(): 
+    def hello():
         return 'Hello, PetFax!'
-
-    # return the app 
+    
+    # register pet blueprint 
+    from . import pet
+    app.register_blueprint(pet.bp)
+    
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
+    
+    # return the app
     return app
